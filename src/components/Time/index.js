f0cc07cae456b8ece1c310601cbbdd98f13e74be
George Czabania <george@czabania.com>
2018-01-09 17:24:06 +1300

    Stop trim at minutes

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -4,11 +4,14 @@ import moment from 'moment'
 import 'moment-duration-format'
 
 export default function Time (props) {
-  const { value, unit, format, trim, ...otherProps } = props
+  const { value, unit, format, trim, stopTrim, ...otherProps } = props
+
+  const formattedTime = moment.duration(value, unit)
+    .format(format, { trim, stopTrim })
 
   return (
     <span {...otherProps}>
-      {moment.duration(value, unit).format(format, { trim })}
+      { formattedTime }
     </span>
   )
 }
@@ -17,11 +20,13 @@ Time.propTypes = {
   format: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired,
   unit: PropTypes.string.isRequired,
-  trim: PropTypes.bool.isRequired
+  trim: PropTypes.bool.isRequired,
+  stopTrim: PropTypes.string
 }
 
 Time.defaultProps = {
   format: 'h:mm:ss',
   unit: 'ms',
-  trim: true
+  trim: true,
+  stopTrim: 'mm'
 }
